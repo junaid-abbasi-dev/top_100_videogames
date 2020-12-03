4f73076e662208445e793e95c98d464ebbad8bf1
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-12-03 17:45:52 -0600

    Change runner file to Cli and create hash of description, release year, ranking, title

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,21 @@
 class Cli
-    
+    attr_reader :games
+    def run
+        puts "Top 100 video games of all time (IGN's report)"
+        Scraper.scrape_games
+        # binding.pry
+        puts "Here is a list of top 100 video games:"
+        list_games
+    end
+
+    def list_games
+        @games = Game.all
+        count = 101
+        games.each do |game|
+            puts "#{count-=1}. #{game.title}"
+            # puts "Rank: #{game.ranking}"
+            # puts "Release year: #{game.released}"
+            # puts "Description: #{game.description}"
+        end
+    end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,17 +4,17 @@ class Scraper
     def self.scrape_games
         html = open(BASE_URL)
         parsed_html = Nokogiri::HTML(html)
-        game_articles = parsed_html.css("section.items article.item")
-        game_articles.each do |game|
+        game_articles = parsed_html.css("article")
+        game_articles.map do |game|
             #binding.pry   
             hash = {
-                title: game.css("div.item-heading a").text,
-                rating: game.css("div.badge-number").text,
-                release_year: game.css("span.item-label-value").text,
-                #description: game.css("div.item-body p").text
+                description: game.css("div.item-body p").first.text.strip,
+                released: game.css("span.item-label-value").first.text.strip,
+                ranking: game.css("div.badge-number").first.text.strip,
+                title: game.css("div.item-heading a").first.text.strip
             }
             Game.new(hash)
         end
-        binding.pry
+        #binding.pry
     end
 end
